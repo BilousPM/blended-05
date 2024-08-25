26b51fde480d15d89666a4d7121b3ffec0158b9e
BilousPM <bilouspm@gmail.com>
2024-08-25 06:54:10 -0600

    Adds comment

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,22 +1,41 @@
-import React, { useState } from 'react';
-import toast from 'react-hot-toast';
-import { BiMailSend } from 'react-icons/bi';
-import styles from './Form.module.css';
+import React, { useState } from "react";
+import toast from "react-hot-toast";
+import { BiMailSend } from "react-icons/bi";
+import styles from "./Form.module.css";
+import { useAddCommentMutation } from "../../redux/commentApi";
+import { Spinner } from "../Spinner/Spinner";
 
 export const Form = () => {
-  const [author, setAuthor] = useState('');
-  const [content, setContent] = useState('');
+  const [addComment, { isLoading }] = useAddCommentMutation();
+
+  const [author, setAuthor] = useState("");
+  const [content, setContent] = useState("");
 
   const onHandleChange = (e) => {
     const { name, value } = e.target;
     console.log(name, value);
+    switch (name) {
+      case "name":
+        setAuthor(value);
+        break;
+      case "text":
+        setContent(value);
+      default:
+        break;
+    }
   };
 
   const onHandleSubmit = (e) => {
     e.preventDefault();
+    if (!author.trim() || !content.trim()) {
+      toast.error("Pleas, all fields must be filled");
+      return;
+    }
+    addComment({ author, content });
+    toast.success("Wait...Comment added");
 
-    setAuthor('');
-    setContent('');
+    setAuthor("");
+    setContent("");
   };
 
   return (
@@ -25,8 +44,8 @@ export const Form = () => {
         <label className={styles.label}>
           <span className={styles.labelName}>Full name</span>
           <input
-            type='text'
-            name='name'
+            type="text"
+            name="name"
             className={styles.input}
             value={author}
             onChange={onHandleChange}
@@ -37,16 +56,22 @@ export const Form = () => {
           <span className={styles.labelName}>Your comment</span>
           <textarea
             className={styles.input}
-            name='text'
-            rows='5'
+            name="text"
+            rows="5"
             value={content}
             onChange={onHandleChange}
           ></textarea>
         </label>
 
-        <button className={styles.formBtn}>
-          <BiMailSend className={styles.icon} />
-          Send
+        <button className={styles.formBtn} disabled={isLoading}>
+          {isLoading ? (
+            <Spinner size="sm" />
+          ) : (
+            <>
+              <BiMailSend className={styles.icon} />
+              Send
+            </>
+          )}
         </button>
       </form>
     </div>
