c4e4a9398295638f29af23e9552ab7d05595b89c
cel3ntano <adriano2024celentano@gmail.com>
2024-08-25 14:14:50 +0200

    task-1_getComments

---
+++ src/redux/commentApi.js
@@ -0,0 +1,17 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const API_ENDPOINT = "/comments";
+const BASE_URL = "https://66b7be7c7f7b1c6d8f1ca717.mockapi.io";
+
+export const commentApi = createApi({
+  reducerPath: "comments",
+  baseQuery: fetchBaseQuery({
+    baseUrl: BASE_URL,
+  }),
+  tagTypes: ["Comments"],
+  endpoints: builder => ({
+    getComments: builder.query({ query: () => API_ENDPOINT }),
+  }),
+});
+
+export const { useGetCommentsQuery } = commentApi;
